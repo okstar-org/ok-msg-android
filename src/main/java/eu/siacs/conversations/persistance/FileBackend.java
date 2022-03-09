snikket-im/snikket-android

don’t query packages before attaching something

src/main/java/eu/siacs/conversations/persistance/FileBackend.java
@@ -1147,19 +1147,13 @@ public class FileBackend {
     }
 
     public Uri getTakePhotoUri() {
+        final String filename = String.format("IMG_%s.%s", IMAGE_DATE_FORMAT.format(new Date()),"jpg");
         File file;
         if (Config.ONLY_INTERNAL_STORAGE) {
-            file =
-                    new File(
-                            mXmppConnectionService.getCacheDir().getAbsolutePath(),
-                            "Camera/IMG_" + IMAGE_DATE_FORMAT.format(new Date()) + ".jpg");
+            final File dcimCache = new File(mXmppConnectionService.getCacheDir(), "Camera");
+            file = new File(dcimCache, filename);
         } else {
-            file =
-                    new File(
-                            getTakePhotoPath()
-                                    + "IMG_"
-                                    + IMAGE_DATE_FORMAT.format(new Date())
-                                    + ".jpg");
+            file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), filename);
         }
         file.getParentFile().mkdirs();
         return getUriForFile(mXmppConnectionService, file);

okstar-org/ok-msg-android

(cherry picked from commit 6fb465f91aae23db422ff2ab9277ec6b0f94a57b)

src/main/java/eu/siacs/conversations/persistance/FileBackend.java
@@ -1190,7 +1190,14 @@ public class FileBackend {
     }
 
     public Uri getTakePhotoUri() {
-        File file = new File(getTakeFromCameraPath() + "IMG_" + fileDateFormat.format(new Date()) + ".jpg");
+        final String filename = String.format("IMG_%s.%s", fileDateFormat.format(new Date()),"jpg");
+        File file;
+        if (STORAGE_INDEX.get() == 1) {
+            final File dcimCache = new File(mXmppConnectionService.getCacheDir(), "Camera");
+            file = new File(dcimCache, filename);
+        } else {
+            file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), filename);
+        }
         file.getParentFile().mkdirs();
         return getUriForFile(mXmppConnectionService, file);
     }